3249a2a4c9bce14eb3b540aa3f22aad5c64b2fe2
Sherard <sherardg@pennmedicine.upenn.edu>
2019-08-28 23:56:41 -0400

    Updated handelbars functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -16,6 +17,9 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 
+var router = express.Router();
+
+require("./config/routes")(router);
 // Configure middleware
 
 // Use morgan logger for logging requests
@@ -23,8 +27,21 @@ app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+app.use(router);
 
 //From Homework instructions:
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
@@ -87,24 +104,10 @@ app.get("/articles", function(req, res) {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+// app.post("/articles/:id"
+app.post("/saved", function(req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
