970096d196c88993cebf8b380d926c87e7a40e39
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-24 21:52:01 -0400

    Updated headline and notes controller

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -1,38 +1,66 @@
+//Controller for our headlines
 //Get scrape scripts
 
-var scrape = require("../scripts/scrape");
-
-//Bring in the Headline and Note mongoose models
-var Headline = require("../models/Headline");
+var db = require("../models");
 
 module.exports = {
-    fetch: function(cb) {
-        scrape(function(data){
-            var articles = data;
-            for (var i = 0; i < articles.length; i++) {
-                articles[i].saved = false;
-            }
+      // Find all headlines, sort them by date, send them back to the user
+  findAll: function(req, res) {
+    db.Headline
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(function(dbHeadline) {
+        res.json(dbHeadline);
+      });
+  },
+  // Delete the specified headline
+  delete: function(req, res) {
+    db.Headline.remove({ _id: req.params.id }).then(function(dbHeadline) {
+      res.json(dbHeadline);
+    });
+  },
+  // Update the specified headline
+  update: function(req, res) {
+    db.Headline.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true }).then(function(dbHeadline) {
+      res.json(dbHeadline);
+    });
+  }
+};
 
-            Headline.collection.insertMany(articles, {ordered:false}, function(err, docs){
-                cb(err, docs);
-            });
-        });
-    },
-    delete: function(query, cb) {
-        Headline.remove(query, cb);
-    },
-    get: function(query, cb) {
-        Headline.find(query)
-        .sort({
-            _id: -1
-        })
-        .exec(function(err, doc) {
-            cb(doc);
-        });
-    },
-    update: function(query, cb) {
-        Headline.update({_id: query._id}, {
-            $set: query
-        }, {}, cb);
-    }
-}
+
+// var scrape = require("../scripts/scrape");
+
+// //Bring in the Headline and Note mongoose models
+// var Headline = require("../models/Headline");
+
+// module.exports = {
+//     fetch: function(cb) {
+//         scrape(function(data){
+//             var articles = data;
+//             for (var i = 0; i < articles.length; i++) {
+//                 articles[i].saved = false;
+//             }
+
+//             Headline.collection.insertMany(articles, {ordered:false}, function(err, docs){
+//                 cb(err, docs);
+//             });
+//         });
+//     },
+//     delete: function(query, cb) {
+//         Headline.remove(query, cb);
+//     },
+//     get: function(query, cb) {
+//         Headline.find(query)
+//         .sort({
+//             _id: -1
+//         })
+//         .exec(function(err, doc) {
+//             cb(doc);
+//         });
+//     },
+//     update: function(query, cb) {
+//         Headline.update({_id: query._id}, {
+//             $set: query
+//         }, {}, cb);
+//     }
+// }
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,31 +1,53 @@
 //Controller for notes
 
-var Note = require("../models/Note");
+var db = require("../models");
+// var Note = require("../models/Note");
 
 module.exports = {
-    get: function (data, cb) {
-        Note.find({
-            _headlineId: data._id
-        }, cb);
-    },
-    save: function(data, cb) {
-        var newNote = {
-            _headlineId: data._id,
-            noteText: data.noteText
-        };
-        Note.create(newNote, function (err, doc) {
-            if (err) {
-                console.log(err);
-            }
-            else{
-                console.log(doc);
-                cb(doc);
-            }
-        });
-    },
-    delete: function(data, cb) {
-        Note.remove({
-            _id: data._id
-        }, cb);
-    }
-}
+
+     // Find one note
+  find: function(req, res) {
+    db.Note.find({ _headlineId: req.params.id }).then(function(dbNote) {
+      res.json(dbNote);
+    });
+  },
+  // Create a new note
+  create: function(req, res) {
+    db.Note.create(req.body).then(function(dbNote) {
+      res.json(dbNote);
+    });
+  },
+  // Delete a note with a given id
+  delete: function(req, res) {
+    db.Note.remove({ _id: req.params.id }).then(function(dbNote) {
+      res.json(dbNote);
+    });
+  }
+};
+
+//     get: function (data, cb) {
+//         Note.find({
+//             _headlineId: data._id
+//         }, cb);
+//     },
+//     save: function(data, cb) {
+//         var newNote = {
+//             _headlineId: data._id,
+//             noteText: data.noteText
+//         };
+//         Note.create(newNote, function (err, doc) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else{
+//                 console.log(doc);
+//                 cb(doc);
+//             }
+//         });
+//     },
+//     delete: function(data, cb) {
+//         Note.remove({
+//             _id: data._id
+//         }, cb);
+//     }
+// }
