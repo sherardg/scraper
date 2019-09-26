0eb4e07335c1b5c1549daa5c22526d759f18c0fa
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-26 00:47:56 -0400

    Updated scrape and handlebars

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -2,21 +2,22 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var scrape = function() {
-    return axios.get("https://alistapart.com/articles").then (function(res){
+    return axios.get("https://6abc.com/").then (function(res){
 
     var $ = cheerio.load(res.data);
     console.log("scraping data")
 
     var articles = [];
 
-    $(".site-main").each(function(i, element){
+    $(".top-stories-expanded").each(function(i, element){
 
-        var head= $(this).find("h2").text().trim();
+        var head= $(this).find(".headline").text().trim();
         var url= $(this).find("a").attr("href");
-        var sum = $(this).find(".entry-content").text().trim(); 
+        var profile= $(this).find("img").attr("src");
+        var sum = $(this).find(".headline-list-item").text().trim(); 
         console.log(url);
 
-        if (head && sum && url) {
+        if (head && sum && profile && url) {
             //Replace regex method to clean up white space
             var headNeat =  head.replace(/(r\n|\n|\r|t|\s+)/gm, " ").trim();
             var sumNeat = sum.replace(/(r\n|\n|\r|t|\s+)/gm, " ").trim();
@@ -24,7 +25,8 @@ var scrape = function() {
             var dataToAdd = {
                 title: headNeat,
                 summary: sumNeat,
-                url: "https://alistapart.com/" + url
+                profile: profile,
+                url: "https://6abc.com/" + url
             };
             articles.push(dataToAdd);
             console.log("Articles", articles);
